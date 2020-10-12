8b7d6700a77148ab8aebfbb26491fdd4879e2b5c
landinij0040 <ijlandin0040@miners.utep.edu>
2020-10-12 15:27:06 -0600

    Made siren_demo

---
+++ demos/siren_demo/stateMachines.h
@@ -0,0 +1,10 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+/* Not sure if i need this */
+//extern char big_state_change; 
+void state_advance();
+void big_state();
+
+#endif // included
+

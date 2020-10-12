0a784c39a05112ae4ff6607e339db236ad58d32c
landinij0040 <ijlandin0040@miners.utep.edu>
2020-10-12 16:21:33 -0600

    Green led and 500hz noise

--- demos/siren_demo/stateMachines.c
+++ demos/siren_demo/stateMachines.c
@@ -2,128 +2,26 @@
 #include "stateMachines.h"
 #include "led.h"
 #include "buzzer.h"
-char big_state_change = 0;
 
-char start() // 100 %
-{
-  buzzer_set_period(1528); // Playing C
-  red_on = 1;
-  return 1;
-}
 
-char s0() // 67%
-{
-  static char state0 = 0;
-  buzzer_set_period(1362); // Playing D 
-  switch (state0) {
-  case 0:
-    red_on = 1;
-    state0 = 1;
-    break;
-  case 1: 
-    red_on = 1;
-    state0 = 2;
-    break;
-  case 2:
-    red_on = 0;
-    state0 = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
-} 
+static int x = 500;
 
-char s1() // 50%
+char freq_up_green_led()
 {
-  static char state1 = 0;
-  buzzer_set_period(1213); // Playing E
-  switch (state1) {
-  case 0:
-    red_on = 1;
-    state1 = 1;
-    break;
-  case 1: 
-    red_on = 0;
-    state1 = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
-}
-
-char s2() // 33%
-{
-  static char state2 = 0;
-  buzzer_set_period(1145); // Playing F
-  switch (state2) {
-  case 0:
-    red_on = 1;
-    state2 = 1;
-    break;
-  case 1: 
-    red_on = 0;
-    state2 = 2;
-    break;
-  case 2:
-    red_on = 0;
-    state2 = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
-}
-
-char end() // 0%
-{
-  red_on = 0;
+  buzzer_set_period(2000000/x);
+  green_on = 1;
   return 1;
 
-}  
+}
 
 void state_advance()		/* alternate between toggling red & green */
 {
   char changed = 0;
   
-  switch(big_state_change){
-  case 0: //Start
-    changed = start();
-    break;
-  case 1: // 67%
-    changed = s0();
-    break;
-  case 2:// 50%
-    changed = s1();
-    break;
-  case 3: // 33%
-    changed = s2();
-    break;
-  case 4: // end
-    changed = end();
-    break;
-  } 
+  changed = freq_up_green_led();
+  
   led_changed = changed;
   led_update();
 }
-void big_state()
-{
-  static char bstate = 0;
-  switch(bstate){
-  case 0:  // For the 100% state
-    big_state_change = 1;
-    bstate = 1;
-    break; 
-  case 1: // For the 67% state
-    big_state_change = 2;
-    bstate = 2;
-    break;
-  case 2: // For the 50% state
-    big_state_change = 3;
-    bstate = 3;
-    break;
-  case 3:// For the 33% state
-    big_state_change = 4;
-    bstate = 4;
-    break;
-  case 4:// For the 0% state
-    big_state_change = 0;
-    bstate = 0;
-    break;
-  }
-}
+
+
--- demos/siren_demo/wdInterruptHandler.c
+++ demos/siren_demo/wdInterruptHandler.c
@@ -3,18 +3,9 @@
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
-  static char blink_count = 0; // the normal blink 
-  static char blink_change = 0; // to dim the led 
-  ++blink_count;
-  ++blink_change;
-  if(blink_count == 1) {
+  static char blink_count = 0; // the normal blink
+  blink_count++;
+  if(blink_count  == 12){
     state_advance();
-    blink_count = 0;
   }
-
-  if(blink_change == 250){
-    big_state();
-    blink_change = 0;
-  }
-  
 }

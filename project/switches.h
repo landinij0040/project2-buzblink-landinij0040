a94b275cafb49cbb3e8c70cb92e921aa1c384a2e
landinij0040 <ijlandin0040@miners.utep.edu>
2020-10-29 10:32:13 -0600

    ORed all the switches into SWITCHES

--- project/switches.h
+++ project/switches.h
@@ -5,7 +5,7 @@
 #define SW2 2           /* switch2 is p2.1 */
 #define SW3 4           /* switch3 is p2.2 */
 #define SW4 8           /* switch4 is p2.3 */
-
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)     /* all the switches*/
 void switch_init();
 void switch_interrupt_handler();
 
